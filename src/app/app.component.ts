b6db8b0a815acf0c64c5dd75952fd3078755f7e1
Ilze Rigerte <irigerte@gmail.com>
2023-04-15 19:18:12 +0300

    structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,39 @@
-import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements AfterViewInit {
+  courses = COURSES;
 
-    courses = COURSES;
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards: QueryList<ElementRef>;
 
+  @ViewChild(HighlightedDirective, { read: HighlightedDirective })
+  highlighted: HighlightedDirective;
 
-    @ViewChildren(CourseCardComponent, {read: ElementRef})
-    cards : QueryList<ElementRef>;
+  constructor() {}
 
+  onToggle(isHighlighted: boolean) {
+    console.log(isHighlighted);
+  }
 
-    constructor() {
+  ngAfterViewInit() {
+    console.log(this.highlighted);
+  }
 
-    }
-
-    ngAfterViewInit() {
-
-    }
-
-    onCourseSelected(course:Course) {
-
-    }
-
+  onCourseSelected(course: Course) {}
 }
